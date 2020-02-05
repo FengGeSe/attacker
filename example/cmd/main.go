e8938ade72565413df92ad11be10c58ba716aec5
wushuaishuai <wushuaishuai@zuoyebang.com>
2020-02-05 21:02:50 +0800

    完善readme.md

---
+++ example/cmd/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	attacker "github.com/FengGeSe/attacker/lib"
+
+	cmd "github.com/FengGeSe/attacker/cmd"
+)
+
+// implements attacker.Task
+type myTask struct{}
+
+var _ attacker.Task = &myTask{}
+
+var count = 0
+
+func (t *myTask) Run() *attacker.Result {
+	rst := attacker.NewResult("流程压测")
+	// A
+	{
+		subRst := rst.SubResult("操作A")
+		subRst.StartTiming()
+		// do something
+		time.Sleep(1 * time.Millisecond)
+		subRst.EndTiming()
+	}
+
+	// B
+	{
+		subRst := rst.SubResult("操作B")
+		subRst.StartTiming()
+		// do something
+		time.Sleep(1 * time.Millisecond)
+		subRst.EndTiming()
+
+		// mock error
+		if count == 1 {
+			subRst.Code = 500
+			subRst.Error = "出错了偶"
+			count++
+			return rst
+		}
+		count++
+	}
+
+	// C
+	{
+		subRst := rst.SubResult("操作C")
+		subRst.StartTiming()
+		// do something
+		time.Sleep(1 * time.Millisecond)
+		subRst.EndTiming()
+	}
+
+	return rst
+}
+
+func main() {
+
+	task := &myTask{}
+
+	cmd.Run(task, os.Stdout)
+}
